ce9b6f0e9c33c45212c51904969e238e7d6aa458
Y姉 <akumakumu@gmail.com>
2025-03-10 05:32:47 +0700

    abstact the get_cursor_pos()

--- get_cursor_win32.py
+++ get_cursor_win32.py
@@ -1,14 +1,21 @@
 import ctypes
 import timeit
 
-user32 = ctypes.windll.user32
+class POINT(ctypes.Structure):
+    _fields_ = [("x", ctypes.c_long), ("y", ctypes.c_long)]
+
+user32 = ctypes.WinDLL('user32', use_last_error=False)
 GetCursorPos = user32.GetCursorPos
-point = (ctypes.c_long * 2)()
+cursor = POINT()
 
-GetCursorPos(point)
-cursor_x, cursor_y = point[0], point[1]
+def get_cursor_pos():
+    GetCursorPos(ctypes.byref(cursor))
+    return cursor.x, cursor.y
 
-execution_time = timeit.timeit(lambda: GetCursorPos(point), number=10000) / 10000 * 1_000_000
+execution_time = timeit.timeit(get_cursor_pos, number=10000) / 10000 * 1_000_000
+cursor_x, cursor_y = get_cursor_pos()
 
+print(type(cursor_x))  # <class 'int'>
+print(type(cursor_y))  # <class 'int'>
 print(f"Cursor position: ({cursor_x}, {cursor_y})")
 print(f"Average time taken: {execution_time:.6f} µs")
